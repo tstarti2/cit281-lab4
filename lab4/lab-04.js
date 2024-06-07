9219e98cb3840b4cbea770b7d953c7eb152cbb0f
tstarti2 <167354176+tstarti2@users.noreply.github.com>
2024-06-07 16:07:04 -0700

    Add files via upload

---
+++ lab4/lab-04.js
@@ -0,0 +1,34 @@
+// Project: CIT 281 Lab 4
+// Author: Tyler Startin
+
+// Require the Fastify framework and instantiate it
+const fastify = require("fastify")();
+// Handle GET verb for / route using Fastify
+// Note use of "chain" dot notation syntax
+fastify.get("/", (request, reply) => {
+  reply
+    .code(200)
+    .header("Content-Type", "text/html; charset=utf-8")
+    .send("<h1>Hello from Lab 4!</h1>");
+});
+// Start server and listen to requests using Fastify
+const listenIP = "localhost";
+const listenPort = 8080;
+fastify.listen(listenPort, listenIP, (err, address) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  console.log(`Server listening on ${address}`);
+});
+
+fastify.get("/name", (request, reply) => {
+  const {first, last} = request.query;
+  const name = !first || !last ? "Hello, Guest" : `Hello, ${first} ${last}`;
+      reply
+      .code(200)
+      .header("Content-Type", "text/html; charset=utf-8")
+      .send(
+        `<h1>${name}</h1>`
+      );
+  });
